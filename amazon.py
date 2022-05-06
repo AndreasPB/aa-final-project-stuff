e0c7e1a5a0462bca4af723c1f51db4227faf6fd7
Andreas Brodersen <43907402+AndreasPB@users.noreply.github.com>
2022-05-06 13:07:05 +0200

    First draft - Imported data to df

---
+++ amazon.py
@@ -0,0 +1,23 @@
+# %%
+import pandas as pd
+import json
+import gzip
+
+# %%
+def parse(path):
+  g = gzip.open(path, 'rb')
+  for l in g:
+    yield json.loads(l)
+
+def getDF(path):
+  i = 0
+  df = {}
+  for d in parse(path):
+    df[i] = d
+    i += 1
+  return pd.DataFrame.from_dict(df, orient='index')
+
+df = getDF('Magazine_Subscriptions.json.gz')
+# %%
+df.head()
+# %%
