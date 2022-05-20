aa3597340b2a973f3c23062d5fe6b704f36d70d4
Andreas Brodersen <43907402+AndreasPB@users.noreply.github.com>
2022-05-20 14:34:43 +0200

    Remove weird duplicated code :shrug:

--- vote_plot.py
+++ vote_plot.py
@@ -4,74 +4,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # %%
-df = pd.read_csv("magazine_reviews_cleanup.tsv", sep='\t')
-df
-
-# %%
-bins = list(range(0, 50+25, 10))
-df["vote"].value_counts(bins=bins).plot(figsize=(9, 3),kind="bar", rot=0)
-plt.ylabel("Amount of reviews")
-plt.xlabel("Vote")
-plt.legend(["Review votes"])
-
-# %%
-bins = list(range(1, 50+25, 10))
-df["vote"].value_counts(bins=bins).plot(figsize=(9, 3),kind="bar", rot=0)
-plt.ylabel("Amount of reviews")
-plt.xlabel("Vote")
-plt.legend(["Review votes"])
-
-# %%
-votes = df["vote"].value_counts()
-no_votes = votes[0].sum()
-has_votes = votes[1:].sum()
-plt.bar(["No votes", "Votes"], [no_votes, has_votes])
-plt.ylabel("Amount of reviews")
-plt.xlabel("Vote")
-
-plt.legend(["Review votes"])
-
-# %%
-bins = list(range(1, 50+25, 10))  # [1, 11, 21, 31, 41, 51, 61, 71]
-
-def split_by_ratings(data: pd.DataFrame) -> list[list]:
-    split_data = []
-    for rating in range(5, 0, -1):
-        single_rating_votes = df.loc[df.rating == rating]["vote"]
-        binned = single_rating_votes.value_counts(bins=bins)
-        split_data.append(binned.to_list())
-    return split_data
-
-split_by_ratings(df)
-
-# %%
-heatmap = np.array(split_by_ratings(df))
-
-bin_labels = ["[1:11]", "[11:21]", "[21:31]", "[31:41]","[41:51]", "[51:61]", "[61:71]"]
-rating_labels = range(5, 0, -1)
-
-fig,ax = plt.subplots()
-im = ax.imshow(heatmap)
-ax.set_yticks(range(len(rating_labels)), lables="rating")
-ax.set_xticks(range(len(bin_labels)), lables="bins")
-ax.set_ylabel("Rating")
-ax.set_xlabel("Bins")
-ax.set_xticklabels(bin_labels)
-ax.set_yticklabels(rating_labels)
-
-for i in range(5):
-    for j in range(len(bins)-1):
-        text = ax.text(j, i, heatmap[i, j], ha="center", va="center")
-
-fig.tight_layout()
-plt.show()
-
-# %%
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-
-# %%
 df = pd.read_csv("data/cleaned_reviews.tsv", sep="\t")
 df
 
@@ -142,5 +74,3 @@ for i in range(5):
 
 fig.tight_layout()
 plt.show()
-
-
