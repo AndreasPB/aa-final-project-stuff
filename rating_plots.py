5c2bde1ffc4527958afcccf61d04906ccf0cb010
Andreas Brodersen <43907402+AndreasPB@users.noreply.github.com>
2022-05-20 14:11:06 +0200

    Add dynamic file handling + better control

--- rating_plots.py
+++ rating_plots.py
@@ -2,7 +2,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-data = pd.read_csv("magazine_reviews_cleanup.tsv", sep="\t")
+data = pd.read_csv("data/cleaned_reviews.tsv", sep="\t")
 
 # %%
 ax = data['rating'].value_counts().sort_index().plot(kind='bar', rot=0)
