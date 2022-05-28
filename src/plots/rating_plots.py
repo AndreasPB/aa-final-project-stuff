6d477f9e899a5721323a37324fb98c56ff4f2ee6
AndreasPB <andreas.p.brodersen@gmail.com>
2022-05-28 15:36:00 +0200

    Export plots to .svg and toggleable exporting

--- src/plots/rating_plots.py
+++ src/plots/rating_plots.py
@@ -2,10 +2,15 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-data = pd.read_csv("../../data/cleaned_reviews.tsv", sep="\t")
 
 # %%
-ax = data['rating'].value_counts().sort_index().plot(kind='bar', rot=0)
+EXPORT_PLOTS = False
+df = pd.read_csv("../../data/cleaned_reviews.tsv", sep="\t")
+
+# %%
+ax = df["rating"].value_counts().sort_index().plot(kind="bar", rot=0)
 ax.set_ylabel("Amount")
 ax.set_xlabel("Rating")
 ax.legend(["Rating"])
+if EXPORT_PLOTS:
+    plt.savefig("../../report/img/rating_distribution.svg", bbox_inches="tight")
