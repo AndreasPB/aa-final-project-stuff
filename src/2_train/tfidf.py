34f819cd757a72dad3b16864c637d7efa7522c4f
Andreas <andreas.p.brodersen@gmail.com>
2022-05-28 17:59:33 +0200

    Add NN MLP Classifier, confusion matrix and balanced acc score (#36)
    * Add NN MLP Classifier, confusion matrix and balanced acc score
    
    * Add new analyse function to TF-IDF script
    
    * Add Youden Index to summary report
    
    * Change parameter to 3.9 syntax

--- src/2_train/tfidf.py
+++ src/2_train/tfidf.py
@@ -7,7 +7,9 @@ import numpy as np
 from sklearn.svm import LinearSVC
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report
+from sklearn.metrics import classification_report, balanced_accuracy_score
+
+from util import summary_report
 
 # %% [markdown]
 # ## Read the data
@@ -19,20 +21,22 @@ df = pd.read_csv("../../data/cleaned_reviews.tsv", sep="\t")
 # ## Defining a helpful review + Splitting the data
 
 # %%
-split = 0.1
+split = 0.01
 
 df["helpful"] = np.where(df.voteSuccess >= split, 1, 0)
 
-x_train, x_test, y_train, y_test = train_test_split(df.reviewText, df.helpful, test_size=0.25, random_state=30)
+x_train, x_test, y_train, y_test = train_test_split(
+    df.reviewText, df.helpful, test_size=0.25, random_state=30
+)
+f"x_train: {x_train.shape}, y_train: {y_train.shape}, x_test: {x_test.shape}, y_test: {y_test.shape}"
 
 # %% [markdown]
 # ## Vectorization with TF-IDF
 
 # %%
-
 vectorizer = TfidfVectorizer(ngram_range=(1, 1), min_df=0.01)
-tfidf_train = vectorizer.fit_transform(x_train.values.astype('U'))
-tfidf_test = vectorizer.transform(x_test.values.astype('U'))
+tfidf_train = vectorizer.fit_transform(x_train.values.astype("U"))
+tfidf_test = vectorizer.transform(x_test.values.astype("U"))
 
 # %% [markdown]
 # ## Fitting
@@ -47,5 +51,21 @@ y_test_pred = clf.predict(tfidf_test)
 # ## Result
 
 # %%
-print("Document-term Matrix(Count Vectorizer) - SVM/SVC")
-print(classification_report(y_test, y_test_pred, target_names=["Unhelpful", "Helpful"]))
+summary_report(y_test, y_test_pred, "Document-term Matrix(TF-IDF Vectorizer) - SVM/SVC")
+
+# %% [markdown]
+# # Neural Network
+
+# %%
+from sklearn.neural_network import MLPClassifier
+
+# Using lbfgs over adam as it is much faster on smaller datasets(1min vs 6min(42k samples on M1 macbook air))
+clf = MLPClassifier(
+    solver="lbfgs", alpha=1e-5, hidden_layer_sizes=(100,), max_iter=1000, random_state=1
+)
+
+clf.fit(tfidf_train, y_train)
+y_test_pred = clf.predict(tfidf_test)
+
+# %%
+summary_report(y_test, y_test_pred, "MLPClassifier")
---
+++ src/2_train/util.py
@@ -0,0 +1,49 @@
+from typing import Optional
+
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import roc_curve
+from sklearn.metrics import classification_report, balanced_accuracy_score
+from sklearn.metrics import confusion_matrix
+from sklearn import metrics
+
+
+def summary_report(y_test: list, y_test_pred: list, msg: Optional[str] = None):
+    if msg:
+        print(msg)
+
+    print(
+        classification_report(
+            y_test, y_test_pred, target_names=["Unhelpful", "Helpful"]
+        ),
+        "\nBalanced acc score: ",
+        round(balanced_accuracy_score(y_test, y_test_pred), 5),
+        "\nBalanced acc score(luck adjusted): ",
+        round(balanced_accuracy_score(y_test, y_test_pred, adjusted=True), 5),
+    )
+
+    eval = evaluate(y_test, y_test_pred)
+
+    print("Youden Index: ", round(eval["Youden"], 5))
+
+    disp = metrics.ConfusionMatrixDisplay(confusion_matrix(y_test, y_test_pred))
+    disp.plot()
+    plt.show()
+
+
+def evaluate(Y, H, beta=1.0):
+    tp = sum((Y == H) * (Y == 1) * 1)
+    tn = sum((Y == H) * (Y == 0) * 1)
+    fp = sum((Y != H) * (Y == 0) * 1)
+    fn = sum((Y != H) * (Y == 1) * 1)
+
+    sensitivity = tp / (tp + fn)
+    specificity = tn / (fp + tn)
+    youden = sensitivity - (1 - specificity)
+
+    result = {}
+    result["sensitivity"] = sensitivity
+    result["specificity"] = specificity
+    result["Youden"] = youden
+
+    return result
